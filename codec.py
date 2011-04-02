d6c5217421c0d9673a0618722fff10b723d4c736
Tyson Bashore <dfunct@gmail.com>
2011-04-01 21:26:21 -0400

    Replaced tabs with spaces.

--- codec.py
+++ codec.py
@@ -1,20 +1,20 @@
 # Parent codec class.
 class Codec:
-	def __init__(self):
-		pass
+   def __init__(self):
+      pass
    
-	@staticmethod
-	def GetExtension(self):
-		pass
+   @staticmethod
+   def GetExtension(self):
+      pass
 
 class DecodingError(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return str(self.value)
+   def __init__(self, value):
+      self.value = value
+   def __str__(self):
+      return str(self.value)
 
 class EncodingError(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return str(self.value)
+   def __init__(self, value):
+      self.value = value
+   def __str__(self):
+      return str(self.value)
