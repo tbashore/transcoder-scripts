cbc802e2e8ffd2db0e955a563326d0d1d4a9231f
Tyson Bashore <dfunct@gmail.com>
2011-04-01 20:23:27 -0400

    Converted tabs to spaces.

--- codecs/apecodec.py
+++ codecs/apecodec.py
@@ -3,51 +3,51 @@ from threading import Thread
 from codec import *
 
 class APECodec(Codec):
-	def __init__(self):
-		Codec.__init__(self)
+   def __init__(self):
+      Codec.__init__(self)
 
-	class DecoderThread(Thread):
-		def __init__(self, source_file):
-			Thread.__init__(self)
-			self.capabilities = {}
-			self.capabilities['input'] = '.ape'
-			self.capabilities['output'] = '.wav'
+   class DecoderThread(Thread):
+      def __init__(self, source_file):
+         Thread.__init__(self)
+         self.capabilities = {}
+         self.capabilities['input'] = '.ape'
+         self.capabilities['output'] = '.wav'
 
-			self.source_file = source_file
-			self.target_file = (os.path.splitext(source_file))[0] + self.capabilities['output']
+         self.source_file = source_file
+         self.target_file = (os.path.splitext(source_file))[0] + self.capabilities['output']
 
-		def GetCapabilities(self):
-			self.capabilities
-		
-		def run(self):
-			if self.source_file.endswith(self.capabilities['input']) == False:
-				raise DecodingError('The source file\'s extension is incompatible with this decoder.')
+      def GetCapabilities(self):
+         self.capabilities
+      
+      def run(self):
+         if self.source_file.endswith(self.capabilities['input']) == False:
+            raise DecodingError('The source file\'s extension is incompatible with this decoder.')
 
-			execstring = 'mac "' + self.source_file + '" "' + self.target_file + '" -d 2>&1 > /dev/null'
-			print 'Decoding "' + self.source_file + '" to "' + self.target_file + '"...'
-			outputfd = os.popen(execstring)
-			output = outputfd.readlines()
-			exitcode = outputfd.close()
+         execstring = 'mac "' + self.source_file + '" "' + self.target_file + '" -d 2>&1 > /dev/null'
+         print 'Decoding "' + self.source_file + '" to "' + self.target_file + '"...'
+         outputfd = os.popen(execstring)
+         output = outputfd.readlines()
+         exitcode = outputfd.close()
 
-	class EncoderThread(Thread):
-		def __init__(self, source_file):
-			Thread.__init__(self)
-			self.capabilities = {}
-			self.capabilities['input'] = '.wav'
-			self.capabilities['output'] = '.ape'
+   class EncoderThread(Thread):
+      def __init__(self, source_file):
+         Thread.__init__(self)
+         self.capabilities = {}
+         self.capabilities['input'] = '.wav'
+         self.capabilities['output'] = '.ape'
 
-			self.source_file = source_file
-			self.target_file = (os.path.splitext(source_file))[0] + self.capabilities['output']
+         self.source_file = source_file
+         self.target_file = (os.path.splitext(source_file))[0] + self.capabilities['output']
 
-		def GetCapabilities(self):
-			self.capabilities
-		
-		def run(self):
-			if self.source_file.endswith(self.capabilities['input']) == False:
-				raise EncodingError('The source file\'s extension is incompatible with this encoder.')
+      def GetCapabilities(self):
+         self.capabilities
+      
+      def run(self):
+         if self.source_file.endswith(self.capabilities['input']) == False:
+            raise EncodingError('The source file\'s extension is incompatible with this encoder.')
 
-			execstring = 'mac "' + self.source_file + '" "' + self.target_file + '" -c3000 2>&1 > /dev/null'
-			print 'Encoding "' + self.source_file + '" to "' + self.target_file + '"...'
-			outputfd = os.popen(execstring)
-			output = outputfd.readlines()
-			exitcode = outputfd.close()
+         execstring = 'mac "' + self.source_file + '" "' + self.target_file + '" -c3000 2>&1 > /dev/null'
+         print 'Encoding "' + self.source_file + '" to "' + self.target_file + '"...'
+         outputfd = os.popen(execstring)
+         output = outputfd.readlines()
+         exitcode = outputfd.close()
--- codecs/mp3codec.py
+++ codecs/mp3codec.py
@@ -3,51 +3,51 @@ from threading import Thread
 from codec import *
 
 class MP3Codec(Codec):
-	def __init__(self):
-		Codec.__init__(self)
+   def __init__(self):
+      Codec.__init__(self)
 
-	class DecoderThread(Thread):
-		def __init__(self, source_file):
-			Thread.__init__(self)
-			self.capabilities = {}
-			self.capabilities['input'] = '.mp3'
-			self.capabilities['output'] = '.wav'
+   class DecoderThread(Thread):
+      def __init__(self, source_file):
+         Thread.__init__(self)
+         self.capabilities = {}
+         self.capabilities['input'] = '.mp3'
+         self.capabilities['output'] = '.wav'
 
-			self.source_file = source_file
-			self.target_file = (os.path.splitext(source_file))[0] + self.capabilities['output']
+         self.source_file = source_file
+         self.target_file = (os.path.splitext(source_file))[0] + self.capabilities['output']
 
-		def GetCapabilities(self):
-			self.capabilities
-		
-		def run(self):
-			if self.source_file.endswith(self.capabilities['input']) == False:
-				raise DecodingError('The source file\'s extension is incompatible with this decoder.')
+      def GetCapabilities(self):
+         self.capabilities
+      
+      def run(self):
+         if self.source_file.endswith(self.capabilities['input']) == False:
+            raise DecodingError('The source file\'s extension is incompatible with this decoder.')
 
-			execstring = 'lame --decode "' + self.source_file + '" "' + self.target_file + '" 2>&1 > /dev/null'
-			print 'Decoding "' + self.source_file + '" to "' + self.target_file + '"...'
-			outputfd = os.popen(execstring)
-			output = outputfd.readlines()
-			exitcode = outputfd.close()
+         execstring = 'lame --decode "' + self.source_file + '" "' + self.target_file + '" 2>&1 > /dev/null'
+         print 'Decoding "' + self.source_file + '" to "' + self.target_file + '"...'
+         outputfd = os.popen(execstring)
+         output = outputfd.readlines()
+         exitcode = outputfd.close()
 
-	class EncoderThread(Thread):
-		def __init__(self, source_file):
-			Thread.__init__(self)
-			self.capabilities = {}
-			self.capabilities['input'] = '.wav'
-			self.capabilities['output'] = '.mp3'
+   class EncoderThread(Thread):
+      def __init__(self, source_file):
+         Thread.__init__(self)
+         self.capabilities = {}
+         self.capabilities['input'] = '.wav'
+         self.capabilities['output'] = '.mp3'
 
-			self.source_file = source_file
-			self.target_file = (os.path.splitext(source_file))[0] + self.capabilities['output']
+         self.source_file = source_file
+         self.target_file = (os.path.splitext(source_file))[0] + self.capabilities['output']
 
-		def GetCapabilities(self):
-			self.capabilities
+      def GetCapabilities(self):
+         self.capabilities
 
-		def run(self):
-			if self.source_file.endswith(self.capabilities['input']) == False:
-				raise DecodingError('The source file\'s extension is incompatible with this decoder.')
+      def run(self):
+         if self.source_file.endswith(self.capabilities['input']) == False:
+            raise DecodingError('The source file\'s extension is incompatible with this decoder.')
 
-			execstring = 'lame -V 2 --add-id3v2 --tc "lame 3.98.4 -V 2" "' + self.source_file + '" "' + self.target_file + '" 2>&1 > /dev/null'
-			print 'Encoding "' + self.source_file + '" to "' + self.target_file + '"...'
-			outputfd = os.popen(execstring)
-			output = outputfd.readlines()
-			exitcode = outputfd.close()
+         execstring = 'lame -V 2 --add-id3v2 --tc "lame 3.98.4 -V 2" "' + self.source_file + '" "' + self.target_file + '" 2>&1 > /dev/null'
+         print 'Encoding "' + self.source_file + '" to "' + self.target_file + '"...'
+         outputfd = os.popen(execstring)
+         output = outputfd.readlines()
+         exitcode = outputfd.close()
